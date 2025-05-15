a4930b24ce7858669e90f5824e3322dc2f33c6df
santiagouribe14 <santiago.uribe@kellogg.northwestern.edu>
2025-05-15 22:42:06 +0000

    Part two of the homework

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get("/places", {:controller => "places", :action => "index"})
-  
 
   # Defines the root path route ("/")
   get("/", { :controller => "places", :action => "index" })
 
   resources "places"
+
+  resources "entries"
 
 end
